1c7ac8b7715bd6ef22c78fd9264c187042e7bf8d
Chris Harding <chrisharding.m@gmail.com>
2021-03-10 10:05:34 -0700

    Adding stash.

--- BucketLists/BucketList.swift
+++ BucketLists/BucketList.swift
@@ -2,24 +2,7 @@
 //  BucketList.swift
 //  BucketLists
 //
-//  Created by Kaleb Page on 3/10/21.
+//  Created by Chris Harding on 3/10/21.
 //
 
 import Foundation
-
-struct BucketList {
-    var owner: String
-    var items: [Item]
-    var color: String
-}
-
-struct Group {
-    var items: [Item]
-}
-
-struct Item {
-    var name: String
-    var description: String
-    var location: String?
-    var goalDate: Date
-}
