38512ad9bb887fff493bb46ec4b0b800a9f18f0d
Chris Harding <chrisharding.m@gmail.com>
2021-03-12 10:13:25 -0700

    Changed some UI

--- BucketLists/ListTableViewCell.swift
+++ BucketLists/ListTableViewCell.swift
@@ -10,25 +10,29 @@ import UIKit
 class ListTableViewCell: UITableViewCell {
 
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var imageViewImage: UIImageView!
     @IBOutlet weak var goalDateLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        contentView.layer.borderColor = UIColor.black.cgColor
+        contentView.layer.borderWidth = 1.0
     }
-
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+    }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
-    func update(with list: List) {
-        nameLabel.text = list.name
-        descriptionLabel.text = list.description
-        locationLabel.text = list.location
+    func update(with list: List, rowNumber: Int) {
+        if list.completed == true {
+            backgroundColor = UIColor.green
+        } else {
+            backgroundColor = UIColor.white
+        }
+        nameLabel.text = "\(rowNumber): \(list.name)"
         goalDateLabel.text = list.goalDate
     }
 }
--- BucketLists/ListTableViewController.swift
+++ BucketLists/ListTableViewController.swift
@@ -9,41 +9,95 @@ import UIKit
 
 class ListTableViewController: UITableViewController {
 
+    @IBOutlet weak var mySegmentedControl: UISegmentedControl!
     var list : [List] = []
+    var listCompleted : [List] = []
+    var bothList : [List] = []
+    
     let defaultlist = [
-        List(name: "Go to Japan", description: "Plant a trip to visit Japan somday", location: "Japan", goalDate: "10/2/22", completed: false),
-        List(name: "The the Grand Canyon", description: "Plant a trip to go visit the Gran Canyon someday", location: "Grand Canyon", goalDate: "12/3/22", completed: false)
+        List(name: "Go to Japan", description: "Take a trip to japan and eat sushi", location: "Japan", goalDate: "10/2/22", completed: false),
+        List(name: "Go to Germany", description: "Take a trip to Germany", location: "Germany", goalDate: "10/2/26", completed: false)
+    ]
+    let defaultlistCompleted = [
+        List(name: "The the Grand Canyon", description: "Plant a trip to go visit the Gran Canyon someday", location: "Grand Canyon", goalDate: "12/3/22", completed: true),
+        List(name: "Finish school", description: "Finish going to school", location: "Grand Canyon", goalDate: "12/3/21", completed: true)
     ]
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if list.isEmpty {
             list = defaultlist
+            listCompleted = defaultlistCompleted
+            bothList = defaultlist + defaultlistCompleted
+            print(bothList)
         }
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return list.count
+        var returnValue = 0
+                
+                switch(mySegmentedControl.selectedSegmentIndex)
+                {
+                case 0:
+                    returnValue = listCompleted.count
+                    break
+                case 1:
+                    returnValue = bothList.count
+                    break
+                    
+                case 2:
+                    returnValue = list.count
+                    break
+                    
+                default:
+                    break
+                }
+                return returnValue
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell", for: indexPath) as! ListTableViewCell
+        let completedlist = listCompleted[indexPath.row]
+        let bothlist = bothList[indexPath.row]
         let newlist = list[indexPath.row]
+        let rowNumber = indexPath.row + 1
         
-        cell.update(with: newlist)
+        switch(mySegmentedControl.selectedSegmentIndex)
+        {
+        case 0:
+            cell.update(with: completedlist, rowNumber: rowNumber)
+            break
+        case 1:
+            cell.update(with: bothlist, rowNumber: rowNumber)
+            break
+            
+        case 2:
+            cell.update(with: newlist, rowNumber: rowNumber)
+            break
+            
+        default:
+            break
+        }
         cell.showsReorderControl = true
 
         return cell
     }
-
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+            return 0
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 48.0;//Choose your custom row height
+    }
+    @IBAction func segmentedControlAction(_ sender: Any) {
+        tableView.reloadData()
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
