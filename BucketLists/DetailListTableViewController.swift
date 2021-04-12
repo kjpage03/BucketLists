55a916ca999246bb77de07772370bf5112df53aa
Kaleb Page <kp17501@ms.wasatch.edu>
2021-04-12 08:56:44 -0600

    Locations Search

--- BucketLists/DetailListTableViewController.swift
+++ BucketLists/DetailListTableViewController.swift
@@ -9,17 +9,20 @@ import UIKit
 import MapKit
 
 class DetailListTableViewController: UITableViewController, UIImagePickerControllerDelegate, UICollectionViewDelegate, UINavigationControllerDelegate, UICollectionViewDataSource {
-
-   
+    
+    
     @IBOutlet weak var nameLabel: UITextField!
     @IBOutlet var descriptionTextView: UITextView!
-    @IBOutlet weak var locationLabel: UITextField!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var completionSwitch: UISwitch!
     @IBOutlet var doneLabel: UIBarButtonItem!
     @IBOutlet var editButton: UIBarButtonItem!
     @IBOutlet weak var descriptionTextField: UITextView!
+    @IBOutlet var setLocationButton: UIButton!
     @IBOutlet weak var imageCollectionView: UICollectionView!
+    @IBOutlet var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
     
     var bucketLists: [BucketList] = []
     var indexOfBucketList: Int = 0
@@ -27,7 +30,7 @@ class DetailListTableViewController: UITableViewController, UIImagePickerControl
     var dataController = DataController()
     var item: Item?
     var editMode: Bool = false
-//    var defaultImageWasRemoved: Bool = false
+    //    var defaultImageWasRemoved: Bool = false
     //Added code begins
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         imageArray.count
@@ -35,8 +38,8 @@ class DetailListTableViewController: UITableViewController, UIImagePickerControl
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = imageCollectionView.dequeueReusableCell(withReuseIdentifier: "pictureCell", for: indexPath) as! ImageCollectionViewCell
-                        cell.imageView.image = self.imageArray[indexPath.row]
-                        return cell
+        cell.imageView.image = self.imageArray[indexPath.row]
+        return cell
     }
     
     var sections = [Section]()
@@ -49,50 +52,62 @@ class DetailListTableViewController: UITableViewController, UIImagePickerControl
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.layer.cornerRadius = 10
         tableView.allowsSelection = false
         self.tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         descriptionTextView.isUserInteractionEnabled = false
         updateItem(item: item)
         
-//        if let image = UIImage(systemName: "photo") {
-//        imageArray.append(image)
-//
-//        }
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        //        locationManager.requestLocation()
+        
+        //        if let image = UIImage(systemName: "photo") {
+        //        imageArray.append(image)
+        //
+        //        }
         
         imageCollectionView.delegate = self
         imageCollectionView.dataSource = self
         
         descriptionTextField.clipsToBounds = true
         descriptionTextField.layer.cornerRadius = 10.0
-        if descriptionTextField.text == "Describe your experience" {
-//            descriptionTextField.backgroundColor = UIColor.lightGray
-            descriptionTextField.isUserInteractionEnabled = true
-        } else {
-//            descriptionTextField.backgroundColor = UIColor.white
-            descriptionTextField.isUserInteractionEnabled = false
-
+        if descriptionTextField.text.count == 0 {
+            descriptionTextField.text = "Describe your experience"
         }
+        //        if descriptionTextField.text == "Describe your experience" {
+        //            descriptionTextField.backgroundColor = UIColor.lightGray
+        //            descriptionTextField.isUserInteractionEnabled = true
+        //        } else {
+        //            descriptionTextField.backgroundColor = UIColor.white
+        //            descriptionTextField.isUserInteractionEnabled = false
+        //
+        //        }
+        
+        
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         if completionSwitch.isOn == true {
-            return 6
+            return 7
         } else {
-        return  5
+            return 4
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
+    
     func updateItem(item: Item?) {
         guard let item = item else {return}
         nameLabel.text = item.name
         descriptionTextView.text = item.description
-        locationLabel.text = item.location
+        //        locationLabel.text = item.location
         datePicker.date = item.goalDate
         completionSwitch.isOn = item.isComplete
         descriptionTextField.text = item.details
@@ -100,24 +115,33 @@ class DetailListTableViewController: UITableViewController, UIImagePickerControl
             imageArray.append(UIImage(data: photoData) ?? UIImage())
         })
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
-
-        guard segue.identifier == "detailUnwind" else {return}
-        let name = nameLabel.text ?? ""
-        let description = descriptionTextView.text ?? ""
-        let location = locationLabel.text ?? ""
-        let goalDate = datePicker.date
-        let completed = completionSwitch.isOn
-        var photos: [Data] = []
-        let details = descriptionTextField.text ?? ""
-        imageArray.forEach { (image) in
-            photos.append(image.pngData() ?? Data())
+        switch segue.identifier {
+        case "LocationSearchVC":
+            let searchViewController = segue.destination as! LocationSearchTableViewController
+            searchViewController.mapView = self.mapView
+            
+        case "detailUnwind":
+            let name = nameLabel.text ?? ""
+            let description = descriptionTextView.text ?? ""
+            //        let location = locationLabel.text ?? ""
+            let goalDate = datePicker.date
+            let completed = completionSwitch.isOn
+            var photos: [Data] = []
+            let details = descriptionTextField.text ?? ""
+            imageArray.forEach { (image) in
+                photos.append(image.pngData() ?? Data())
+            }
+            item = Item( name: name, description: description, location: "location", goalDate: goalDate, isComplete: completed, photos: photos, details: details)
+            //        bucketLists[indexOfBucketList].items[indexOfItem] = item!
+            //        dataController.saveData(lists: bucketLists)
+        default :
+            break
         }
-        item = Item( name: name, description: description, location: location, goalDate: goalDate, isComplete: completed, photos: photos, details: details)
-//        bucketLists[indexOfBucketList].items[indexOfItem] = item!
-//        dataController.saveData(lists: bucketLists)
+        
+
         
     }
     
@@ -127,15 +151,15 @@ class DetailListTableViewController: UITableViewController, UIImagePickerControl
             editButton.image = UIImage(systemName: "checkmark")
             nameLabel.isUserInteractionEnabled = true
             descriptionTextView.isUserInteractionEnabled = true
-            locationLabel.isUserInteractionEnabled = true
+            //            locationLabel.isUserInteractionEnabled = true
             datePicker.isUserInteractionEnabled = true
             descriptionTextField.isUserInteractionEnabled = true
             
             nameLabel.borderStyle = UITextField.BorderStyle.roundedRect
-//            descriptionLabel.borderStyle = UITextField.BorderStyle.roundedRect
-            locationLabel.borderStyle = UITextField.BorderStyle.roundedRect
-//            descriptionTextField.backgroundColor = UIColor.lightGray
-
+            //            descriptionLabel.borderStyle = UITextField.BorderStyle.roundedRect
+            //            locationLabel.borderStyle = UITextField.BorderStyle.roundedRect
+            //            descriptionTextField.backgroundColor = UIColor.lightGray
+            
             editMode = true
         }
         else if editMode == true {
@@ -143,30 +167,30 @@ class DetailListTableViewController: UITableViewController, UIImagePickerControl
             editButton.image = nil
             nameLabel.isUserInteractionEnabled = false
             descriptionTextView.isUserInteractionEnabled = false
-            locationLabel.isUserInteractionEnabled = false
+            //            locationLabel.isUserInteractionEnabled = false
             datePicker.isUserInteractionEnabled = false
             descriptionTextField.isUserInteractionEnabled = false
             
             nameLabel.borderStyle = UITextField.BorderStyle.none
-//            descriptionLabel.borderStyle = UITextField.BorderStyle.none
-            locationLabel.borderStyle = UITextField.BorderStyle.none
-//            descriptionTextField.backgroundColor = UIColor.white
-
+            //            descriptionLabel.borderStyle = UITextField.BorderStyle.none
+            //            locationLabel.borderStyle = UITextField.BorderStyle.none
+            //            descriptionTextField.backgroundColor = UIColor.white
+            
             
             editMode = false
         }
     }
-   // override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-       // if indexPath.row == 2 {
-       //     return 512
-       // } else{
-       //     return UITableView.automaticDimension
-       // }
-   // }
-     @IBAction func CompletionSwitch(_ sender: Any) {
-        tableView.reloadData()
-     }
+    // override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    // if indexPath.row == 2 {
+    //     return 512
+    // } else{
+    //     return UITableView.automaticDimension
+    // }
+    // }
     
+    @IBAction func CompletionSwitch(_ sender: Any) {
+        tableView.reloadData()
+    }
     
     @IBAction func editingChanged(_ sender: Any) {
         if nameLabel.text?.count != 0 {
@@ -176,6 +200,26 @@ class DetailListTableViewController: UITableViewController, UIImagePickerControl
         }
     }
     
+    @IBAction func setLocationButtonClicked(_ sender: Any) {
+        let ac = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Current Location", style: .default, handler: { (action) in
+            //do stuff
+        }))
+        ac.addAction(UIAlertAction(title: "Find a Location", style: .default, handler: { (action) in
+            //do stuff
+            
+//            guard let nav = self.navigationController, let top = nav.topViewController else {
+//                return
+//            }
+//
+//            nav.popViewController(animated: true)
+            self.performSegue(withIdentifier: "LocationSearchVC", sender: nil)
+            
+
+        }))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(ac, animated: true, completion: nil)
+    }
     
     
     @IBAction func addImageButton(_ sender: Any) {
@@ -190,14 +234,43 @@ class DetailListTableViewController: UITableViewController, UIImagePickerControl
         guard let selectedImage = info[.editedImage] as? UIImage else {
             fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
         }
-//        if !defaultImageWasRemoved {
-//            imageArray.remove(at: 0)
-//            defaultImageWasRemoved = true
-//        }
+        //        if !defaultImageWasRemoved {
+        //            imageArray.remove(at: 0)
+        //            defaultImageWasRemoved = true
+        //        }
         imageArray.append(selectedImage)
         dismiss(animated: true, completion: nil)
         imageCollectionView.reloadData()
         //dataSource.apply()
     }
     
+    @IBAction func unwindFromSearch(unwindSegue: UIStoryboardSegue) {
+        print("Success")
+    }
+    
 }
+
+extension DetailListTableViewController : CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error:: \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    
+}
+
+
---
+++ BucketLists/LocationSearchTableViewController.swift
@@ -0,0 +1,113 @@
+//
+//  LocationSearchTableViewController.swift
+//  BucketLists
+//
+//  Created by Kaleb Page on 4/6/21.
+//
+
+import UIKit
+import MapKit
+
+protocol HandleMapSearch {
+    func dropPinZoomIn(placemark: MKPlacemark)
+}
+
+class LocationSearchTableViewController: UITableViewController {
+    
+    var matchingItems: [MKMapItem] = []
+    var mapView: MKMapView? = nil
+    var resultSearchController: UISearchController? = nil
+    var selectedPin: MKPlacemark? = nil
+    var handleMapSearchDelegate:HandleMapSearch? = nil
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable2") as! LocationSearchTableViewController
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for a location..."
+        navigationItem.searchController = resultSearchController
+        
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        definesPresentationContext = true
+        locationSearchTable.mapView = mapView
+        
+        locationSearchTable.handleMapSearchDelegate = self
+ 
+    }
+    
+}
+
+extension LocationSearchTableViewController : UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        guard let mapView = mapView,
+                let searchBarText = searchController.searchBar.text else { return }
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchBarText
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        search.start { (response, _) in
+            guard let response = response else {
+                return
+            }
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
+        }
+    }
+}
+
+extension LocationSearchTableViewController {
+
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchingItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        let selectedItem = matchingItems[indexPath.row].placemark
+        cell.textLabel?.text = selectedItem.name
+        let address = "\(selectedItem.thoroughfare ?? ""), \(selectedItem.locality ?? ""), \(selectedItem.subLocality ?? ""), \(selectedItem.administrativeArea ?? ""), \(selectedItem.postalCode ?? ""), \(selectedItem.country ?? "")"
+        cell.detailTextLabel?.text = address
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedItem = matchingItems[indexPath.row].placemark
+        handleMapSearchDelegate?.dropPinZoomIn(placemark: selectedItem)
+        dismiss(animated: true, completion: nil)
+        
+        performSegue(withIdentifier: "searchUnwind", sender: nil)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let detailView = segue.destination as! DetailListTableViewController
+        detailView.mapView = self.mapView
+    }
+    
+}
+
+extension LocationSearchTableViewController: HandleMapSearch {
+    func dropPinZoomIn(placemark: MKPlacemark) {
+        selectedPin = placemark
+        
+        mapView!.removeAnnotations(mapView!.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.name
+        if let city = placemark.locality, let state = placemark.administrativeArea {
+            annotation.subtitle = "\(city), \(state)"
+        }
+        mapView?.addAnnotation(annotation)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: placemark.coordinate, span: span)
+        mapView!.setRegion(region, animated: true)
+    }
+}
